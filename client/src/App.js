0be8e8055097781ec6ca8cdb94ad0f0332fff56a
Neil Solon <neilsolon@Helens-iPhone.localdomain>
2020-03-13 18:11:35 -0700

    retrieve info from axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
-import './styles/main.css';
-import Header from './components/Header';
+import React, { Component } from "react";
+import "./styles/main.css";
+import axios from "axios";
+import Header from "./components/Header";
 import Map from "./components/Map";
 import Foodtrucks from "./components/Foodtrucks";
-import Roulettes from './components/Roulettes';
-
+import Roulettes from "./components/Roulettes";
 
 class App extends Component {
+  state = {
+    info: []
+  };
 
+  // API_KEY
+// const apiKey = "AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo";
 
+  componentDidMount() {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/http://data.streetfoodapp.com/1.1/schedule/vancouver"
+      )
+      .then(res => {
+        this.setState({
+          info: res.data
+        });
+        console.log(res);
+      });
+  }
 
   render() {
     return (
       <div className="App">
         <Header />
         <Map />
-        <Foodtrucks />
-        
+        <Foodtrucks info={this.state.info} />
+
         {/* <Roulettes /> */}
       </div>
     );
@@ -24,5 +41,3 @@ class App extends Component {
 }
 
 export default App;
-
-
--- client/src/components/Foodtrucks.js
+++ client/src/components/Foodtrucks.js
@@ -1,11 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Select from "react-select";
-import sampleTruck from '../assets/images/sample-food-truck.jpeg'
+import sampleTruck from "../assets/images/sample-food-truck.jpeg";
 import ReactModal from "react-modal";
-import Details from './Details';
-
-
-
+import Details from "./Details";
 
 const options = [
   { value: "featured", label: "Featured" },
@@ -13,82 +10,145 @@ const options = [
   { value: "new", label: "New" }
 ];
 export default class Foodtrucks extends Component {
-    state = {
-        selectedOption: null,
-        showModal: false,
-        details: []
+  constructor(props) {
+    super(props);
 
-    }
-
-    handleOpenModal = () => {
-      this.setState({showModal: true})
-    }
-
-    handleCloseModal = () => {
-      this.setState({showModal: false})
-    }
-
-    handleChange = selectedOption => {
-        this.setState({selectedOption});
-        console.log('Selected option: ', selectedOption);
+    this.state = {
+      selectedOption: null,
+      showModal: false
+      // info: []
+      // details: [],
     };
+  }
 
+  handleOpenModal = e => {
+    e.preventDefault();
+    this.setState({ showModal: true });
+  };
 
-    render() {
-        const  {selectedOption} = this.state;
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  };
+
+  handleChange = selectedOption => {
+    this.setState({ selectedOption });
+    console.log("Selected option: ", selectedOption);
+  };
+
+  render() {
+    const { selectedOption } = this.state;
+
+    // check if info props is loading
+    // if (!undefined) {
+    //   // return (<div>hey</div>)
+    // } else {
+    //   return <p className="loading">Loading..</p>;
+    // }
+
+    let foodTruck;
+    if (this.props.info.vendors) {
+      const obj = Object.entries(this.props.info.vendors);
+      obj.length = 3;
+      foodTruck = obj.map(array => {
+        console.log(array);
+
         return (
-          <>
-            <Select
-              className="select"
-              value={selectedOption}
-              onChange={this.handleChange}
-              options={options}
-            />
-
-            <div className="foodtrucks">
-             
-
-              <div className="foodtrucks__card">
-                <div className="foodtrucks__card-segment">
-                  <button
-                    className="foodtrucks__card-segment-name"
-                    onClick={this.handleOpenModal}
-                  >
-                    Dahbest Foodtruck
-                  </button>
-                  <img
-                    className="foodtrucks__card-segment-logo"
-                    src={sampleTruck}
-                    alt="food truck logo"
-                  />
-                </div>
-                <div className="foodtrucks__card-segment">
-                  <h4 className="foodtrucks__card-segment-address">
-                    455 Granville
-                  </h4>
-                  <h4 className="foodtrucks__card-segment-hours">
-                    9 a.m. - 10 p.m.
-                  </h4>
-                </div>
-                <div className="foodtrucks__card-segment">
-                  <p className="foodtrucks__card-segment-description">
-                    Yummiest food truck in the Whole Wide World
-                  </p>
-                </div>
-              </div>
-              <ReactModal
-                ariaHideApp={false}
-                isOpen={this.state.showModal}
-                contentLabel="Minimal Modal Example"
+          <div className="foodtrucks__card">
+            <div className="foodtrucks__card-segment">
+              <a
+                href="#"
+                className="foodtrucks__card-segment-name"
+                onClick={this.handleOpenModal}
               >
-                <Details
-                  closeModalNow={this.handleCloseModal}
-                  details={this.props.details}
-                  // updateTheState={this.props.updateTheState}
-                />
-              </ReactModal>
+                <h2>{array[0]}</h2>
+                <p>{array[1].description_short}</p>
+              </a>
             </div>
-          </>
+          </div>
         );
+        // for (let i = 0; i < 11; i++) {
+        //   return array[i][1].name;
+        // }
+        // console.log("name", name);
+      });
     }
+    // let result = []
+
+    // let namer = obj.forEach(item => {
+    //   result = [...result, ...Object.values(item.data.vendors.name)];
+    // });
+
+    // {
+    //   this.props.info.map(truck => {
+    //     console.log("truck", truck);
+    //   });
+    // }
+
+    // let trucks;
+    // for (let truck in this.props.info.vendors) {
+    //   console.log(truck);
+    // }
+
+    // if (this.props.info.vendors !== undefined) {
+    //   const keys = Object.keys(this.props.info.vendors);
+    //   console.log(keys);
+    //   for (const key of keys) {
+    //     console.log(key);
+    //   }
+    // }
+
+    return (
+      <>
+        {foodTruck}
+        <Select
+          className="select"
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={options}
+        />
+
+        <div className="foodtrucks">
+          <div className="foodtrucks__card">
+            <div className="foodtrucks__card-segment">
+              <button
+                className="foodtrucks__card-segment-name"
+                onClick={this.handleOpenModal}
+              >
+                {/* {console.log("name here", { name })} */}
+              </button>
+              <img
+                className="foodtrucks__card-segment-logo"
+                src={sampleTruck}
+                alt="food truck logo"
+              />
+            </div>
+            <div className="foodtrucks__card-segment">
+              <h4 className="foodtrucks__card-segment-address">
+                455 Granville
+              </h4>
+              <h4 className="foodtrucks__card-segment-hours">
+                9 a.m. - 10 p.m.
+              </h4>
+            </div>
+            <div className="foodtrucks__card-segment">
+              <p className="foodtrucks__card-segment-description">
+                Yummiest food truck in the Whole Wide World
+              </p>
+            </div>
+          </div>
+          <ReactModal
+            ariaHideApp={false}
+            isOpen={this.state.showModal}
+            contentLabel="Minimal Modal Example"
+          >
+            <Details
+              closeModalNow={this.handleCloseModal}
+              // details={this.props.details}
+              // updateTheState={this.props.updateTheState}
+            />
+          </ReactModal>
+        </div>
+      </>
+    );
+  }
 }
